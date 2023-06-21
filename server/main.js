e1b2cb968b157f3440f4645cea517617148390fe
Tatiana_K <ottertut@gmail.com>
2023-06-21 21:44:11 +0300

    Second commit

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
+//Meteor.startup(() => {
   // code to run on server at startup
+//});
+
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
+
+Meteor.startup(() => {
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task'
+    ].forEach(insertTask)
+  }
 });
+
